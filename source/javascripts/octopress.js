64b260f51c94f5d2aec33a08cd953bccf3868977
Jake McCrary <jake@jakemccrary.com>
2016-03-27 13:49:53 -0500

    i don't use gists for code.

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -33,24 +33,7 @@ function addSidebarToggler() {
   if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
 }
 
-function addCodeLineNumbers() {
-  if (navigator.appName === 'Microsoft Internet Explorer') { return; }
-  $('div.gist-highlight').each(function(code) {
-    var tableStart = '<table><tbody><tr><td class="gutter">',
-        lineNumbers = '<pre class="line-numbers">',
-        tableMiddle = '</pre></td><td class="code">',
-        tableEnd = '</td></tr></tbody></table>',
-        count = $('.line', code).length;
-    for (var i=1;i<=count; i++) {
-      lineNumbers += '<span class="line-number">'+i+'</span>\n';
-    }
-    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
-    $(code).html(table);
-  });
-}
-
 $('document').ready(function() {
-  addCodeLineNumbers();
   getNav();
   addSidebarToggler();
 });
