986a3037cc325694356f69e17c9b0c673e109fcf
Jake McCrary <jake@jakemccrary.com>
2020-05-04 21:17:09 -0500

    separate clearing cloudflare and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
+
 bundle exec rake clean gen_deploy
 rm -rf docs/*
 cp -R public/* docs/
-
-confirm() {
-    # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure? [y/N]} " response
-    case "$response" in
-        [yY][eE][sS]|[yY])
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
 
 git add docs
 git commit -am 'deploy'
 git push
 
-if [ "x${CLOUDFLARE_AUTH_KEY}x" != "xx" ]; then
-    confirm "Do you want to clear Cloudflare's cache of the main files? [y/N]" && \
-    curl -X DELETE "https://api.cloudflare.com/client/v4/zones/05e4518e68577e8be55ec1039e4ff3f4/purge_cache" \
-         -H "X-Auth-Email:jake@jakemccrary.com" \
-         -H "X-Auth-Key:${CLOUDFLARE_AUTH_KEY}" \
-         -H "Content-Type:application/json" \
-         --data '{"files":["http://jakemccrary.com/", "http://jakemccrary.com/index.html", "http://jakemccrary.com/atom.xml", "http://jakemccrary.com/feed.json", "http://www.jakemccrary.com/", "http://www.jakemccrary.com/index.html", "http://www.jakemccrary.com/atom.xml", "http://www.jakemccrary.com/feed.json", "https://jakemccrary.com/", "https://jakemccrary.com/index.html", "https://jakemccrary.com/atom.xml", "https://jakemccrary.com/feed.json", "https://www.jakemccrary.com/", "https://www.jakemccrary.com/index.html", "https://www.jakemccrary.com/atom.xml", "https://www.jakemccrary.com/feed.json", "https://jakemccrary.com/blog/archives/", "https://jakemccrary.com/blog/archives/index.html", "https://www.jakemccrary.com/blog/archives/", "https://www.jakemccrary.com/blog/archives/index.html", "http://jakemccrary.com/blog/archives/", "http://jakemccrary.com/blog/archives/index.html", "http://www.jakemccrary.com/blog/archives/", "http://www.jakemccrary.com/blog/archives/index.html"]}'
-fi
+./clear-cloudflare.sh
