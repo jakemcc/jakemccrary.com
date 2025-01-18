2e888b9a60f471599aa03aa26e8142cb051ac5d6
Jake McCrary <jake@jakemccrary.com>
2025-01-18 14:22:54 -0600

    render files under posts

--- bb/blog/render.clj
+++ bb/blog/render.clj
@@ -1,12 +1,14 @@
 (ns blog.render
   (:require [babashka.fs :as fs]
-            [markdown.core :as markdown]))
+            [markdown.core :as markdown]
+            [selmer.parser]))
 
 (def output-dir "output")
+(def template-dir "templates")
 
 (defn posts []
   (when (fs/exists? "posts")
-    (fs/glob "posts" "*.markdown")))
+    (fs/glob "posts" "**.markdown")))
 
 (defn pre-process-markdown [markdown]
   (-> markdown
@@ -33,14 +35,32 @@
         post-process-markdown)))
 
 (defn load-post [file]
-  (markdown->html file))
+  (assoc (markdown->html file)
+         :path file))
+
+(defn load-template [post]
+  (let [template (or (-> post :metadata :layout) "default")
+        file (fs/file template-dir (str template ".html"))]
+    (if (fs/exists? file)
+      (slurp file)
+      (throw (Exception. (str "template not found: " (str file)))))))
+
+(defn output-file [post]
+  (let [sub-dirs (->> (fs/components (:path post))
+                      (rest)
+                      (butlast))
+        name (clojure.string/replace (fs/file-name (:path post))
+                                     ".markdown"
+                                     ".html")]
+    (apply fs/file (concat [output-dir] sub-dirs [name]))))
 
 (defn write-post! [file]
-  (let [post (load-post file)]
-    (spit (fs/file output-dir (clojure.string/replace (fs/file-name file)
-                                                      ".markdown"
-                                                      ".html"))
-          (:html post))))
+  (let [post (load-post file)
+        template (load-template post)
+        out-file (output-file post)]
+    (fs/create-dirs (fs/parent out-file))
+    (spit out-file
+          (selmer.parser/render template {:body (:html post)}))))
 
 (defn render [& args]
   (println "Rendering")
@@ -48,3 +68,22 @@
     (fs/create-dir output-dir))
   (doseq [post (posts)]
     (write-post! post)))
+
+(comment
+  (render)
+  
+  (def a-post (first (posts)))
+  (load-post a-post)
+  (output-file (load-post a-post))
+
+  (fs/components a-post)
+
+
+  (load-template (load-post (first (posts))))
+
+  (write-post! (first (posts)))
+
+
+
+;;
+)
