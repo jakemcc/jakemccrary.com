0b52c8f2b8a0da40139a27e460ab8f5bb36ba187
Jake McCrary <jake@jakemccrary.com>
2025-01-19 13:24:52 -0600

    watch.sh to auto generate

--- watch.sh
+++ watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while sleep 0.1; do rg --files -t clojure -t markdown -t html | entr -d /opt/homebrew/bin/bb render; done
+while sleep 0.1; do rg bb templates posts --files -t clojure -t markdown -t html | entr -d time /opt/homebrew/bin/bb render; done
