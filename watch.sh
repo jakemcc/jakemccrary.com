7237aefb2b2d0ed926db746801b162d993b3b2ea
Jake McCrary <jake@jakemccrary.com>
2025-01-19 19:16:55 -0600

    watch css too

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while sleep 0.1; do 
-    rg bb templates posts --files -t clojure -t markdown -t html \
+    rg bb templates posts --files -t css -t clojure -t markdown -t html \
         | entr -d -s 'rm -rf output/*; time /opt/homebrew/bin/bb render';
 done
