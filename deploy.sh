9efca1e976896c5c87a076f803e2027e2107acd4
Jake McCrary <jake@jakemccrary.com>
2025-02-02 18:50:48 -0600

    probably have somewhat working deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+set -ex 
+
 if [ -z ${WAS_SOURCED+x} ]; then
     echo "source Envfile"
     exit 1
 fi
 
-rm -rf public/*
-bundle exec rake clean gen_deploy
+rm -rf output/*
+bb test
+bb render
 rm -rf docs/*
-cp -R public/* docs/
+cp -R output/* docs/
 
 git add docs
 git commit -am 'deploy'
-git push
+# git push
 
-./clear-cloudflare.sh
+# ./clear-cloudflare.sh
