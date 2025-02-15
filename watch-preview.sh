2bf843a857acae8517eb74b402c27a26d72b703b
Jake McCrary <jake@jakemccrary.com>
2025-02-15 13:07:23 -0600

    send notification when rendering happens

--- watch-preview.sh
+++ watch-preview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while sleep 0.5; do 
     rg bb templates source --files -t css -t clojure -t markdown -t html \
-        | entr -d -s 'rm -rf output/*; time /opt/homebrew/bin/bb render --preview true';
+        | entr -d -s 'rm -rf output/*; time /opt/homebrew/bin/bb render --preview true && terminal-notifier -message "Rendering complete"';
 done
--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while sleep 0.5; do 
     rg bb templates source --files -t css -t clojure -t markdown -t html \
-        | entr -d -s 'rm -rf output/*; time /opt/homebrew/bin/bb render';
+        | entr -d -s 'rm -rf output/*; time /opt/homebrew/bin/bb render && terminal-notifier -title "BB Watch" -message "Rendering complete"';
 done
